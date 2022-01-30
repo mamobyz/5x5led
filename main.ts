575322b81db3d3b67960e09718f280c2591d3368
Don Jing <mamobyz@users.noreply.github.com>
2022-01-30 19:40:07 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+function blink () {
+	music.playMelody("G G E F F D C D E C A3 B3 C4 C#4 ", 120);
+}
 basic.showLeds(`
     . . # . .
     . . # . .
@@ -10,5 +13,9 @@ basic.showIcon(IconNames.ArrowEast)
 basic.forever(function () {
     basic.clearScreen()
     basic.pause(1000)
-    basic.setLedColor(0x7f00ff)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.setLedColor(0x00ff00)
+    blink(); 
+    basic.setLedColor(0xff0000)
+    basic.showIcon(IconNames.Heart)
 })
